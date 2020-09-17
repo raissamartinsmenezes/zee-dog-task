e747bed7a4f752411e2f4538fc96e2bf8839a51c
raissamartinsmenezes <raissamartinsdemenezes@gmail.com>
2020-09-17 16:51:06 -0300

    chore(webpack): change output filename

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
     entry: './src/index.js',
     output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'index_bundle.js',
+    filename: 'main.js',
     publicPath: '/'
     },
     module: {
