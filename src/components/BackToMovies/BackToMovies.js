43f018fea04681be5b14ea0126e7b9d04acce148
raissamartinsmenezes <raissamartinsdemenezes@gmail.com>
2020-09-17 02:01:03 -0300

    feat(components): add navigation components

---
+++ src/components/BackToMovies/BackToMovies.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./BackToMovies.css";
+
+const BackToMovies = (props) => {
+    // const { movieDirector } = props;
+    return <p className="back-to-movies">← Voltar para Filmes</p>;
+}
+
+export default BackToMovies;
---
+++ src/components/GoToFavorites/GoToFavorites.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./GoToFavorites.css";
+
+const GoToFavorites = (props) => {
+    // const { buttonContent } = props;
+    return <button className="go-to-favorites">Meus Favoritos</button>;
+}
+
+export default GoToFavorites;
