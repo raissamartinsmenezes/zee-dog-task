8d20f4f2a44fafbc228ac2999fb95b4e582a6655
raissamartinsmenezes <raissamartinsdemenezes@gmail.com>
2020-09-17 16:37:37 -0300

    feat(component): add favorite behavior to button

--- src/components/FavoriteButton/FavoriteButton.js
+++ src/components/FavoriteButton/FavoriteButton.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import "./FavoriteButton.css";
 import redHeart from "../../../assets/red-heart.svg";
 import greyHeart from "../../../assets/grey-heart.svg";
 
 const FavoriteButton = () => {
-    return <img src={redHeart} className="favorite-button"/>;
-}
+    const [isFavorite, setIsFavorite] = useState(false);
+
+    const onHandleClickButton = () => {
+        setIsFavorite(true);
+        if (isFavorite === true) setIsFavorite(false);
+    }   
+
+    return (
+        <button onClick={onHandleClickButton}>
+            <img src={isFavorite ? redHeart : greyHeart} className="favorite-button"/>
+        </button>
+    );
+};
 
 export default FavoriteButton;
