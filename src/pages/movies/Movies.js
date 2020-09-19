51422b7261128f275bc5e7154397e4883b6069dc
raissamartinsmenezes <raissamartinsdemenezes@gmail.com>
2020-09-19 01:55:07 -0300

    feat(component): add condition to show skeleton loading
    add movies list map

--- src/pages/movies/Movies.js
+++ src/pages/movies/Movies.js
@@ -6,16 +6,22 @@ import PageTitle from "../../components/PageTitle/PageTitle";
 import GoToFavorites from "../../components/GoToFavorites/GoToFavorites";
 import CardMovie from "../../components/CardMovie/CardMovie";
 import { fetchMovies } from "../../store/slices/moviesSlice";
+import SkeletonLoading from "../../components/SkeletonLoading/SkeletonLoading";
 
 const Movies = () => {
     const movies = useSelector(state => state.movies);
+    const { moviesList, isLoading } = movies;
     const dispatch = useDispatch();
 
     useEffect(() => {
         dispatch(fetchMovies());
-    }, [])
+    }, []);
 
-    console.log(movies);
+    console.log(isLoading);
+
+    // const countSkeleton = () => {
+    //    return moviesList.length ? moviesList.length : 4; 
+    // }
 
     return (
         <>
@@ -26,21 +32,19 @@ const Movies = () => {
                 <GoToFavorites />
             </div>
             <div className="card-box">
-                <CardMovie 
-                    hasMovieDescription={true}
-                />
-                <CardMovie 
-                    hasMovieDescription={true}
-                />
-                <CardMovie 
-                    hasMovieDescription={true}
-                />
-                <CardMovie 
-                    hasMovieDescription={true}
-                />
+                {isLoading ? <SkeletonLoading /> :
+                moviesList.map((movie) => (
+                    <CardMovie
+                        key={movie.id}
+                        title={movie.title}
+                        director={movie.director} 
+                        description={movie.description}
+                        hasMovieDescription={true}
+                    />
+                ))}
             </div>
         </>
-    )
+    );
 }
 
 export default Movies;
