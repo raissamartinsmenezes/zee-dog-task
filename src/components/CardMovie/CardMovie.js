39c9fd253be45aae713b2556d5afc0872593ab81
raissamartinsmenezes <raissamartinsdemenezes@gmail.com>
2020-09-19 01:51:17 -0300

    feat(component): add props to card movie

--- src/components/CardMovie/CardMovie.js
+++ src/components/CardMovie/CardMovie.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./CardMovie.css";
+import { useSelector } from "react-redux";
 
 import MovieName from "../MovieName/MovieName";
 import MovieDirector from "../MovieDirector/MovieDirector";
@@ -7,29 +8,30 @@ import MovieDescription from "../MovieDescription/MovieDescription";
 import FavoriteButton from "../FavoriteButton/FavoriteButton";
 import ShowMore from "../ShowMore/ShowMore";
 
-const lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-
 const CardMovie = (props) => {
-    const { hasMovieDescription } = props;
-    return (
-            <div className="card-movie">
+    const { hasMovieDescription, title, director, description } = props;
+
+    return ( 
+        <div className="card-movie">
+            <div>
                 <div className="card-movie--title">
                     <div>
                         <MovieName 
-                            movieName="Nome do Filme"
+                             movieName={title}
                         />
-                        <MovieDirector 
-                            movieDirector="Diretor"
+                        <MovieDirector e
+                            movieDirector={director}
                         />
                     </div>
                     <FavoriteButton />
                 </div>
-                {hasMovieDescription ? 
-                <MovieDescription 
-                    movieDescription={lorem}
-                /> : null}
-                <ShowMore />
+                { hasMovieDescription ? 
+                <MovieDescription
+                    movieDescription={description}
+                /> : null }
             </div>
+            <ShowMore />
+        </div>
     );
 };
 
