df3f5e9318dc7d79cd2b695a661e8ccf6cf2e18b
raissamartinsmenezes <raissamartinsdemenezes@gmail.com>
2020-09-21 14:48:18 -0300

    feat(favorite): add button logic to make the heart fill or empty

--- src/components/FavoriteButton/FavoriteButton.js
+++ src/components/FavoriteButton/FavoriteButton.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { useDispatch } from "react-redux";
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import "./FavoriteButton.css";
 import redHeart from "../../../assets/red-heart.svg";
 import greyHeart from "../../../assets/grey-heart.svg";
@@ -8,17 +8,35 @@ import { addMovieToFavorites, removeMovieFromFavorites } from "../../store/slice
 
 const FavoriteButton = (props) => {
     const { movie } = props;
+    const favorites = useSelector(state => state.favorites);
     const [isFavorite, setIsFavorite] = useState(false);
     const dispatch = useDispatch();
+
+    console.log(favorites);
+
+    // const favoritedMovie = {
+    //     movie: movie,
+    //     isMovieFavorite: true,
+    // };
+
+    console.log(movie);
 
     const onHandleClickButton = () => {
         setIsFavorite(true);
         dispatch(addMovieToFavorites(movie));
-        if (isFavorite === true) {
+        if(isFavorite === true) {
             setIsFavorite(false);
             dispatch(removeMovieFromFavorites(movie));
         } 
     } 
+    
+    useEffect(() => {
+        if(favorites.find((favorite) => favorite.id === movie.id)) {
+            return setIsFavorite(true);
+        } else {
+            return setIsFavorite(false);
+        }
+    }, []);
 
     return (
         <button onClick={onHandleClickButton}>
