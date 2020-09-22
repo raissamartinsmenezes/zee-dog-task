ce266e69d22385fd0656787b46e9e8c902df0216
raissamartinsmenezes <raissamartinsdemenezes@gmail.com>
2020-09-22 17:34:42 -0300

    refactor(error): add error handling

--- src/pages/movies/Movies.js
+++ src/pages/movies/Movies.js
@@ -5,6 +5,7 @@ import PageTitle from "../../components/PageTitle/PageTitle";
 import GoToFavorites from "../../components/GoToFavorites/GoToFavorites";
 import CardMovie from "../../components/CardMovie/CardMovie";
 import SkeletonLoading from "../../components/SkeletonLoading/SkeletonLoading";
+import ErrorBox from "../../components/ErrorBox/ErrorBox";
 
 import { fetchMovies } from "../../store/slices/moviesSlice";
 
@@ -27,18 +28,22 @@ const Movies = () => {
                 />
                 <GoToFavorites />
             </div>
-            <div className="card-box">
-                {isLoading ? 
-                TOTAL_OF_SKELETONS.map((skeleton) => (<SkeletonLoading />)) 
-                :
-                moviesList.map((movie) => (
-                    <CardMovie
-                        key={movie.id}
-                        movie={movie}
-                        hasMovieDescription={true}
-                    />
-                ))}
-            </div>
+            {
+                error || !moviesList.length
+                ? <ErrorBox errorContent="📽️ Oops! An unexpected error occurred while trying to load the movies, try later!" />
+                : <div className="card-box">
+                    {isLoading ? 
+                    TOTAL_OF_SKELETONS.map((skeleton, index) => (<SkeletonLoading key={index}/>)) 
+                    :
+                    moviesList.map((movie) => (
+                        <CardMovie
+                            key={movie.id}
+                            movie={movie}
+                            hasMovieDescription={true}
+                        />
+                    ))}
+                </div>
+            }
         </>
     );
 };
