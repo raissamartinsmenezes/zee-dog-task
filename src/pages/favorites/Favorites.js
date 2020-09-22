191dc1b761a360fa7250234afc1c33cd93766629
raissamartinsmenezes <raissamartinsdemenezes@gmail.com>
2020-09-22 18:03:11 -0300

    refactor: remove console log

--- src/pages/favorites/Favorites.js
+++ src/pages/favorites/Favorites.js
@@ -8,7 +8,6 @@ import ErrorBox from "../../components/ErrorBox/ErrorBox";
 
 const Favorites = () => {
     const favorites = useSelector(state => state.favorites);
-    console.log(favorites);
 
     return (
         <>
